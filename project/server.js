a9be85aedbc2e169074d4220ea19c5505c0ef358
industlabs <57274248+industlabs@users.noreply.github.com>
2020-08-13 17:20:05 -0500

    Update server.js

--- project/server.js
+++ project/server.js
@@ -16,8 +16,8 @@ app.set('views', path.join(__dirname, '/views/'));
 app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/' }));
 app.set('view engine', 'hbs');
 
-app.listen(3000, () => {
-    console.log('Express server started at port : 3000');
+app.listen(5000, () => {
+    console.log('Express server started at port : 5000');
 });
 
 app.use('/employee', employeeController);
